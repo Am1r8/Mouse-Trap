d7676b6c7651361991a7c1e8ee7269373135b071
AlPHA__830 <68626539+CoachAlpha@users.noreply.github.com>
2022-04-26 23:01:47 -0400

    main functionality added

---
+++ script.js
@@ -0,0 +1,37 @@
+const block = document.getElementById("mole");
+const timer = document.getElementById("time");
+const score1 = document.getElementById("score");
+let time = 500;
+let score = 0;
+
+function resize() {
+    let size = 25 + Math.random() * 100;
+    block.style.transform = `scale(${size}%, ${size}%`;
+}
+
+function relocate() {
+    let x = 5 + Math.random() * 90;
+    let y = 15 + Math.random() * 45;
+    block.style.left = x + "%";
+    block.style.top = y + "%";
+    score++;
+    score1.innerText = "Score: " + score;
+}
+
+function timerfunc() {
+    time--;
+    timer.innerText = time;
+    if (time == 0) {
+        time = 500;
+    }
+}
+
+setInterval(timerfunc, 0.1);
+
+function reset() {
+    time = 500;
+}
+
+block.addEventListener("click", resize);
+block.addEventListener("click", relocate);
+block.addEventListener("click", reset);
